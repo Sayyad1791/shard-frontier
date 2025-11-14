f7e1b5d142ca5e18c76733523ac0e7f1c1884675
Sayyad1791 <sayyad0601791@gmail.com>
2025-11-14 18:43:53 -0500

    Update index.js

--- x1-backend/src/index.js
+++ x1-backend/src/index.js
@@ -1,3 +1,4 @@
+import x1Routes from "./x1Routes.js";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -8,6 +9,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/x1", x1Routes);
 
 // Simple root route
 app.get("/", (req, res) => {
