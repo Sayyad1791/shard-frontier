91e6f39d004e8f228b97cb0d6072b3b39655cf6e
Sayyad1791 <sayyad0601791@gmail.com>
2025-11-17 20:18:57 -0500

    Update db.js

--- x1-backend/src/db.js
+++ x1-backend/src/db.js
@@ -1,6 +1,10 @@
-import pkg from 'pg';
+import pkg from "pg";
 const { Pool } = pkg;
 
-export const pool = new Pool({
-connectionString: process.env.DATABASE_URL
+// Create a connection pool to Postgres on Railway
+const pool = new Pool({
+connectionString: process.env.DATABASE_URL,
 });
+
+// Export it as the default export so "import db from './db.js'" works
+export default pool;
